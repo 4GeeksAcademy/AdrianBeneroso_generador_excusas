4a0853414a154996cb47a71e18052bb0ea092aed
AdrianBene <adrianbeneroso@hotmail.com>
2025-03-23 22:55:00 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+ let who =['Mi madre','Mi hija','El gato','Mi mujer'];
+ let action =['comio','rompio','guardo','salto'];
+ let what =['mi proyecto','mi ordenador','el coche','el telefono'];
+ let when = ['despues de clase','cuando yo me fui a dormir','mientras hacia ejercicio','durante la comida']
+
+ var who_random = Math.floor(Math.random()* who.length);
+ var action_random = Math.floor(Math.random()* who.length);
+ var what_random = Math.floor(Math.random()* who.length );
+ var when_random = Math.floor(Math.random()* who.length );
+
+ document.getElementById("excuse").innerHTML =  who[who_random] +" " +action[action_random] + " " + what[what_random]+" "+ when[when_random]
+
+
+
 };
